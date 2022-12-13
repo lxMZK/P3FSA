7d4005e62f7b2d439d5e63da754400fcc78f7839
lxMZK <alexmmizak@gmail.com>
2022-12-13 10:15:25 -0500

    home style

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -6,7 +6,7 @@ export default function Home() {
   const {currentUser} = useContext(AuthContext)
 
   return (
-    <div className="content toggleV">
+    <div className="homeContent toggleV">
       <div className="home">
         <h1>Hello {currentUser? (currentUser.fname || currentUser.username)+'!' : 'World!'}</h1>
         <h3>
