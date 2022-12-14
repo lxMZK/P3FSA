00425ad52461e6da9d22ced200dc24e846458cde
lxMZK <alexmmizak@gmail.com>
2022-12-13 21:33:40 -0500

    test

--- index.js
+++ index.js
@@ -17,56 +17,42 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
 }
 
-app.post('/auth/register', (req, res) => {
-  pool.query(
-    'SELECT * FROM users WHERE email = $1 OR username = $2',
-    [req.body.email, req.body.username],
-    async (err, data) => {
-      if (err) return res.json(err);
-      if (data.rows.length != 0)
-        return res.status(409).json('User already exists');
-
-      const salt = bcrypt.genSaltSync(10);
-      const hash = bcrypt.hashSync(req.body.password, salt);
-
-      await pool.query(
-        'INSERT INTO users(username, email, password, fname, lname) VALUES ($1, $2, $3, $4, $5)',
-        [
-          req.body.username,
-          req.body.email,
-          hash,
-          req.body.fname,
-          req.body.lname,
-        ]
-      );
-    }
+app.post('/auth/register', async (req, res) => {
+  await pool.query(
+    'INSERT INTO users(username, email, password, fname, lname) VALUES ($1, $2, $3, $4, $5)',
+    [req.body.username, req.body.email, hash, req.body.fname, req.body.lname]
   );
 });
 
 app.post('/auth/login', (req, res) => {
-  pool.query(
-    'SELECT * FROM users WHERE username = $1',
-    [req.body.username],
-    (err, data) => {
-      if (err) return res.json(err);
-      if (data.rows.length === 0)
-        return res.status(404).json('User not found!');
+  pool
+    .connect()
+    .query(
+      'SELECT * FROM users WHERE username = $1',
+      [req.body.username],
+      (err, data) => {
+        if (err) return res.json(err);
+        if (data.rows.length === 0)
+          return res.status(404).json('User not found!');
 
-      const auth = bcrypt.compareSync(req.body.password, data.rows[0].password);
+        const auth = bcrypt.compareSync(
+          req.body.password,
+          data.rows[0].password
+        );
 
-      if (!auth) return res.status(400).json('Wrong password');
+        if (!auth) return res.status(400).json('Wrong password');
 
-      const token = jwt.sign({ id: data.rows[0].id }, 'jwtkey');
-      const { password, ...other } = data.rows[0];
+        const token = jwt.sign({ id: data.rows[0].id }, 'jwtkey');
+        const { password, ...other } = data.rows[0];
 
-      res
-        .cookie('access_token', token, {
-          httpOnly: true,
-        })
-        .status(200)
-        .json(other);
-    }
-  );
+        res
+          .cookie('access_token', token, {
+            httpOnly: true,
+          })
+          .status(200)
+          .json(other);
+      }
+    );
 });
 
 app.post('/auth/logout', (req, res) => {
